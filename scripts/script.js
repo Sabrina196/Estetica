8e8e87d1e5736dde9d24b734719ff2ac523569cc
Sabrina196 <sabrinapatri96@gmail.com>
2022-06-11 19:42:19 -0300

    Relleno de texto en Index y Somos

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
-consoleText(['Capacitaciones 2022', 'Descuentos Exclusivos', 'Bonus 2x1'], 'text',['HotPink','MediumOrchid','DarkViolet']);
+consoleText(['Capacitaciones 2022', 'Descuentos Exclusivos', 'Bonus 2x1'], 'text',['Violet','MediumOrchid','DarkViolet']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
